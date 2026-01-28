5d5c03640ee778af142f51e1054f7807f4b3a8d1
u22u <orabo2acc@gmail.com>
2026-01-28 16:55:37 +0000

    Add cloudinary_storage app

--- codestar/settings.py
+++ codestar/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Cloudinary storage app (placed immediately after staticfiles)
+    'cloudinary_storage',
+
     # Project apps
     'blog',
     'about',
