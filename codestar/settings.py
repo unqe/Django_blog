e4e632a632659a4eb0ddb396d114559d36a71eaf
u22u <orabo2acc@gmail.com>
2026-01-28 10:59:05 +0000

    commit 123456789abcdef.

--- codestar/settings.py
+++ codestar/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'replace-me-with-a-secure-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
