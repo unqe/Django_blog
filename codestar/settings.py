faf3ac189f099170cbbbd7b4a644953fb1f2eaa6
u22u <orabo2acc@gmail.com>
2026-01-28 11:12:08 +0000

    Fix corrupted settings.py: clean up, set DEBUG=False and allow .herokuapp.com

--- codestar/settings.py
+++ codestar/settings.py
@@ -35,8 +35,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'codestar.urls'
 
-TEMPLATES = [
-    {
+TEMPLATES = [] = [
+        {
+    
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
@@ -96,43 +97,35 @@ TEMPLATES = [
 
         WSGI_APPLICATION = 'codestar.wsgi.application'
 
-        # Database
-        # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-        DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': BASE_DIR / 'db.sqlite3',
-            }
-        }
+import os
+from pathlib import Path
 
-        # Password validation
-        # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-        AUTH_PASSWORD_VALIDATORS = [
-            {
-                'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-            },
-            {
-                'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-            },
-            {
-                'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-            },
-            {
-                'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-            },
-        ]
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-        # Internationalization
-        LANGUAGE_CODE = 'en-us'
-        TIME_ZONE = 'UTC'
-        USE_I18N = True
-        USE_TZ = True
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'replace-me-with-a-secure-key')
 
-        # Static files (CSS, JavaScript, Images)
-        STATIC_URL = 'static/'
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
 
-        DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Allow local hosts for development and Heroku domain for deployment
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
+# Application definition
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'blog',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -160,10 +153,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'codestar.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -171,10 +162,8 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -190,25 +179,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
