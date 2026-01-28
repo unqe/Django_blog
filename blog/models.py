5d2ef107d7834f1de77f702224b1395682af5f81
u22u <orabo2acc@gmail.com>
2026-01-28 13:13:41 +0000

    Add Meta ordering and __str__ to Post

--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,9 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title}"
