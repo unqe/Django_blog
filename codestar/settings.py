a55d2a0ba58f17b5bf4eaaa18c6c8b404042c291
u22u <orabo2acc@gmail.com>
2026-01-28 11:00:23 +0000

    commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'replace-me-with-a-secure-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
