4c778585896d8940fcd14cf95dad897b5101718e
u22u <orabo2acc@gmail.com>
2026-01-28 16:07:02 +0000

    Handle POST in post_detail to save comments

--- blog/views.py
+++ blog/views.py
@@ -29,7 +29,17 @@ def post_detail(request, slug):
 	# Comments for this post
 	comments = post.comments.all().order_by("-created_on")
 	comment_count = post.comments.filter(approved=True).count()
-	comment_form = CommentForm()
+
+	if request.method == "POST":
+		comment_form = CommentForm(data=request.POST)
+		if comment_form.is_valid():
+			comment = comment_form.save(commit=False)
+			comment.author = request.user
+			comment.post = post
+			comment.save()
+			comment_form = CommentForm()
+	else:
+		comment_form = CommentForm()
 
 	return render(
 		request,
